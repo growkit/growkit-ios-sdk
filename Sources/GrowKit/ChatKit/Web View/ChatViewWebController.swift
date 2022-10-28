e2b8b9e9745185c7a96f585d34c5885033b2642a
Khalid2021-Dev <82113560+Khalid2021-Dev@users.noreply.github.com>
2022-10-28 11:42:36 -0400

    userContentController

--- Sources/GrowKit/ChatKit/Web View/ChatViewWebController.swift
+++ Sources/GrowKit/ChatKit/Web View/ChatViewWebController.swift
@@ -27,82 +27,17 @@ class ChatWebViewController: UIViewController, WKUIDelegate {
         let myURL = URL(string:"http://localhost:8000/chat")
         let myRequest = URLRequest(url: myURL!)
         webView.load(myRequest)
+        let contentController = self.webView.configuration.userContentController
+        contentController.add(self, name: "toggleMessageHandler")
     }
 }
 
-//
-//class ChatKitWebViewModel: ObservableObject {
-//    @Published var isLoading: Bool = false
-//    @Published var canGoBack: Bool = false
-//    @Published var shouldGoBack: Bool = false
-//    @Published var title: String = ""
-//
-//    var url: String
-//
-//    init(url: String) {
-//        self.url = url
-//    }
-//}
-//// self.webView.configuration.userContentController.add(a,name: "observer")
-//class ChatKitWebViewContainer: UIViewController, UIViewRepresentable {
-//    @ObservedObject var webViewModel: ChatKitWebViewModel
-//    func makeCoordinator() -> ChatKitWebViewContainer.Coordinator {
-//        Coordinator(self, webViewModel)
-//    }
-//
-//    func makeUIView(context: Context) -> WKWebView {
-//        guard let url = URL(string: webViewModel.url) else {
-//            return WKWebView()
-//        }
-//
-//        let request = URLRequest(url: url)
-//
-//        let _webView = WKWebView()
-//        _webView.isOpaque = false
-//        _webView.backgroundColor = UIColor(Color("Primary"))
-//        _webView.navigationDelegate = context.coordinator
-//
-//        _webView.load(request)
-//        //self.webView = _webView
-//        return _webView
-//    }
-//
-//    func updateUIView(_ uiView: WKWebView, context: Context) {
-//        if webViewModel.shouldGoBack {
-//            uiView.goBack()
-//            webViewModel.shouldGoBack = false
-//        }
-//    }
-//}
-//
-//extension ChatKitWebViewContainer {
-//    class Coordinator: NSObject, WKNavigationDelegate, WKScriptMessageHandler {
-//        @ObservedObject private var webViewModel: ChatKitWebViewModel
-//        private let parent: ChatKitWebViewContainer
-//
-//        init(_ parent: ChatKitWebViewContainer, _ webViewModel: ChatKitWebViewModel) {
-//            self.parent = parent
-//            self.webViewModel = webViewModel
-//
-//        }
-//
-//        func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
-//            webViewModel.isLoading = true
-//           // webView.configuration.userContentController.add(self,name: "observer")
-//        }
-//
-//        func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-//            webViewModel.isLoading = false
-//            webViewModel.title = webView.title ?? ""
-//            webViewModel.canGoBack = webView.canGoBack
-//        }
-//
-//        func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
-//            webViewModel.isLoading = false
-//        }
-//
-//        func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
-//            print("From WebView GrowKit: ",message.body)
-//        }
-//    }
-//}
+extension ChatWebViewController: WKScriptMessageHandler{
+    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+        guard let dict = message.body as? [String : AnyObject] else {
+            return
+        }
+
+        print(dict)
+    }
+}
