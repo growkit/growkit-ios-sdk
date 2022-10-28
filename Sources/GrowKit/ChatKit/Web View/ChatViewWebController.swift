ba4540254c776b7fdbc61a503ff4c8e5a6f88743
Khalid2021-Dev <82113560+Khalid2021-Dev@users.noreply.github.com>
2022-10-28 11:00:10 -0400

    FirstTry Of WebView With ChatKit

---
+++ Sources/GrowKit/ChatKit/Web View/ChatViewWebController.swift
@@ -0,0 +1,88 @@
+//
+//  ChatViewWebController.swift
+//  
+//
+//  Created by Khalid Fawal on 2022-10-28.
+//
+
+import Foundation
+import SwiftUI
+import WebKit
+
+class ChatKitWebViewModel: ObservableObject {
+    @Published var isLoading: Bool = false
+    @Published var canGoBack: Bool = false
+    @Published var shouldGoBack: Bool = false
+    @Published var title: String = ""
+
+    var url: String
+
+    init(url: String) {
+        self.url = url
+    }
+}
+
+struct ChatKitWebViewContainer: UIViewRepresentable {
+    @ObservedObject var webViewModel: ChatKitWebViewModel
+
+    func makeCoordinator() -> WebViewContainer.Coordinator {
+        let a = Coordinator(self, webViewModel)
+        webView.configuration.userContentController.add(a,name: "observer")
+    }
+
+    func makeUIView(context: Context) -> WKWebView {
+        guard let url = URL(string: webViewModel.url) else {
+            return WKWebView()
+        }
+
+        let request = URLRequest(url: url)
+
+        let webView = WKWebView()
+        webView.isOpaque = false
+        webView.backgroundColor = UIColor(Color("Primary"))
+        webView.navigationDelegate = context.coordinator
+
+        webView.load(request)
+
+        return webView
+    }
+
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        if webViewModel.shouldGoBack {
+            uiView.goBack()
+            webViewModel.shouldGoBack = false
+        }
+    }
+}
+
+extension WebViewContainer {
+    class Coordinator: NSObject, WKNavigationDelegate, WKScriptMessageHandler {
+        @ObservedObject private var webViewModel: ChatKitWebViewModel
+        private let parent: WebViewContainer
+
+        init(_ parent: WebViewContainer, _ webViewModel: WebViewModel) {
+            self.parent = parent
+            self.webViewModel = webViewModel
+
+        }
+
+        func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+            webViewModel.isLoading = true
+           // webView.configuration.userContentController.add(self,name: "observer")
+        }
+
+        func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+            webViewModel.isLoading = false
+            webViewModel.title = webView.title ?? ""
+            webViewModel.canGoBack = webView.canGoBack
+        }
+
+        func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+            webViewModel.isLoading = false
+        }
+
+        func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+            print("From WebView GrowKit: ",message.body)
+        }
+    }
+}
